f42d38c56d9d2a7cba38e92079fed35e0952853d
Lisa DeBona <annalisa.quiamco@gmail.com>
2020-10-22 01:36:16 -0600

    updated jquery library

--- inc/scripts.php
+++ inc/scripts.php
@@ -6,10 +6,14 @@ function bellaworks_scripts() {
 	wp_enqueue_style( 'bellaworks-style', get_stylesheet_uri() );
 
 	wp_deregister_script('jquery');
-		wp_register_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js', false, '3.5.1', false);
+		wp_register_script('jquery', 'https://code.jquery.com/jquery-3.3.1.min.js', false, '3.5.1', false);
 		wp_enqueue_script('jquery');
 
-	
+	wp_enqueue_script( 
+			'jquery-migrate','https://code.jquery.com/jquery-migrate-1.4.1.min.js', 
+			array(), '20200713', 
+			false 
+		);
 
 	wp_enqueue_script( 
 			'bellaworks-blocks', 
