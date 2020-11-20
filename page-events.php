bfda3cf5eee009dadc72d57c213c1b4f60807387
Lisa DeBona <annalisa.quiamco@gmail.com>
2020-11-20 00:17:58 -0700

    Build Event Spage parent page

--- page-events.php
+++ page-events.php
@@ -35,129 +35,133 @@ $currentPageLink = get_permalink();
 		);
 		$posts = new WP_Query($args);
 		if( $posts->have_posts() ) { ?>
-		<div class="filter-wrapper optionsnum3">
-			<div class="wrapper">
-				
-				<div class="filter-inner">
-					<div class="filterbytxt">Filter By:</div>
-					<div class="flexwrap">
 
-						<?php if ( do_shortcode('[facetwp facet="event_space_num_guests"]') ) { ?>
-						<div class="select-wrap">
-							<label>Number of Guests</label>
-								<?php echo do_shortcode('[facetwp facet="event_space_num_guests"]'); ?>
-						</div>
-						<?php } ?>
+			<?php /* Filter Options */ ?>
+			<div class="filter-wrapper optionsnum3">
+				<div class="wrapper">
+					
+					<div class="filter-inner">
+						<div class="filterbytxt">Filter By:</div>
+						<div class="flexwrap">
 
-						<?php if ( do_shortcode('[facetwp facet="event_space_venue"]') ) { ?>
-						<div class="select-wrap">
-							<label>Venue Size</label>
-							<?php echo do_shortcode('[facetwp facet="event_space_venue"]'); ?>
-						</div>
-						<?php } ?>
+							<?php if ( do_shortcode('[facetwp facet="event_space_num_guests"]') ) { ?>
+							<div class="select-wrap">
+								<label>Number of Guests</label>
+									<?php echo do_shortcode('[facetwp facet="event_space_num_guests"]'); ?>
+							</div>
+							<?php } ?>
 
-						<?php if ( do_shortcode('[facetwp facet="event_space_loctype"]') ) { ?>
-						<div class="select-wrap">
-							<label>Indoor / Outdoor</label>
-							<?php echo do_shortcode('[facetwp facet="event_space_loctype"]'); ?>
+							<?php if ( do_shortcode('[facetwp facet="event_space_venue"]') ) { ?>
+							<div class="select-wrap">
+								<label>Venue Size</label>
+								<?php echo do_shortcode('[facetwp facet="event_space_venue"]'); ?>
+							</div>
+							<?php } ?>
+
+							<?php if ( do_shortcode('[facetwp facet="event_space_loctype"]') ) { ?>
+							<div class="select-wrap">
+								<label>Indoor / Outdoor</label>
+								<?php echo do_shortcode('[facetwp facet="event_space_loctype"]'); ?>
+							</div>
+							<?php } ?>
 						</div>
-						<?php } ?>
 					</div>
+
 				</div>
-
 			</div>
-		</div>
 
+			<?php /* Entries */ ?>
+			<div class="post-type-entries columns3 <?php echo $postype ?>">
+				<div id="data-container">
+					<div class="posts-inner">
+						<div class="flex-inner">
+							<?php $i=1; while ( $posts->have_posts()) : $posts->the_post(); ?>
+								<?php
+								$id = get_the_ID();
+								$title = get_the_title();
+								$pagelink = get_permalink();
+								$short_description = get_field("short_description");
+								$thumbImage = get_field("featured_image");
+								$space_features = get_field("space_features");
+								$space_variables = array('capacity','area_size','type');
 
-		<div class="post-type-entries columns3 <?php echo $postype ?>">
-			<div id="data-container">
-				<div class="posts-inner">
-					<div class="flex-inner">
-						<?php $i=1; while ( $posts->have_posts()) : $posts->the_post(); ?>
-							<?php
-							$id = get_the_ID();
-							$title = get_the_title();
-							$pagelink = get_permalink();
-							$short_description = get_field("short_description");
-							$thumbImage = get_field("featured_image");
-							$space_features = get_field("space_features");
-							$space_variables = array('capacity','area_size','type');
+								?>
+								<div class="postbox animated fadeIn <?php echo ($thumbImage) ? 'has-image':'no-image' ?>">
+									<div class="inside">
 
-							?>
-							<div class="postbox animated fadeIn <?php echo ($thumbImage) ? 'has-image':'no-image' ?>">
-								<div class="inside">
-
-									<a href="<?php echo $pagelink ?>" class="photo wave-effect js-blocks">
-										<?php if ($thumbImage) { ?>
-											<span class="imagediv" style="background-image:url('<?php echo $thumbImage['sizes']['medium_large'] ?>')"></span>
-											<img src="<?php echo $thumbImage['url']; ?>" alt="<?php echo $thumbImage['title'] ?>" class="feat-img" style="display:none;">
-											<img src="<?php echo $blank_image ?>" alt="" class="feat-img placeholder">
-										<?php } else { ?>
-											<span class="imagediv"></span>
-											<img src="<?php echo $blank_image ?>" alt="" class="feat-img placeholder">
-										<?php } ?>
-										<span class="boxTitle">
-											<span class="twrap">
-												<span class="t1"><?php echo $title ?></span>
+										<a href="<?php echo $pagelink ?>" class="photo wave-effect js-blocks">
+											<?php if ($thumbImage) { ?>
+												<span class="imagediv" style="background-image:url('<?php echo $thumbImage['sizes']['medium_large'] ?>')"></span>
+												<img src="<?php echo $thumbImage['url']; ?>" alt="<?php echo $thumbImage['title'] ?>" class="feat-img" style="display:none;">
+												<img src="<?php echo $blank_image ?>" alt="" class="feat-img placeholder">
+											<?php } else { ?>
+												<span class="imagediv"></span>
+												<img src="<?php echo $blank_image ?>" alt="" class="feat-img placeholder">
+											<?php } ?>
+											<span class="boxTitle">
+												<span class="twrap">
+													<span class="t1"><?php echo $title ?></span>
+												</span>
 											</span>
-										</span>
-										<?php include( locate_template('images/wave-svg.php') ); ?>
-									</a>
+											<?php include( locate_template('images/wave-svg.php') ); ?>
+										</a>
 
-									<div class="details">
-										<div class="info">
-											<h3 class="event-name"><?php echo $title ?></h3>
+										<div class="details">
+											<div class="info">
+												<h3 class="event-name"><?php echo $title ?></h3>
 
-											<?php if ($short_description) { ?>
-											<div class="short-description">
-												<?php echo $short_description ?>
-											</div>
-											<?php } ?>
-
-											<?php if ($space_features) { ?>
-											<div class="space-features">
-												<?php foreach ($space_features as $k=>$v) { ?>
-													<?php if ( in_array($k, $space_variables) ) {  ?>
-														<?php 
-															if ($k=='type') {
-																if( isset($v['value']) && $v['value']!='na' ) {
-																	$v = ( isset($v['label']) && $v['label'] ) ? $v['label'] : $v;
-																} else {
-																	$v = '';
-																}
-															} ?>	
-														
-														<?php if ($v) { ?>
-															<span class="sf"><?php echo $v ?></span>
-														<?php } ?>
-														
-													<?php } ?>
+												<?php if ($short_description) { ?>
+												<div class="short-description">
+													<?php echo $short_description ?>
+												</div>
 												<?php } ?>
-											</div>
-											<?php } ?>
 
-											<div class="button">
-												<a href="<?php echo $pagelink ?>" class="btn-sm"><span>See Details</span></a>
+												<?php if ($space_features) { ?>
+												<div class="space-features">
+													<?php foreach ($space_features as $k=>$v) { ?>
+														<?php if ( in_array($k, $space_variables) ) {  ?>
+															<?php 
+																if ($k=='type') {
+																	if( isset($v['value']) && $v['value']!='na' ) {
+																		$v = ( isset($v['label']) && $v['label'] ) ? $v['label'] : $v;
+																	} else {
+																		$v = '';
+																	}
+																} ?>	
+															
+															<?php if ($v) { ?>
+																<span class="sf"><?php echo $v ?></span>
+															<?php } ?>
+															
+														<?php } ?>
+													<?php } ?>
+												</div>
+												<?php } ?>
+
+												<div class="button">
+													<a href="<?php echo $pagelink ?>" class="btn-sm"><span>See Details</span></a>
+												</div>
+												
 											</div>
-											
 										</div>
 									</div>
 								</div>
-							</div>
-						<?php $i++; endwhile; wp_reset_postdata(); ?>
+							<?php $i++; endwhile; wp_reset_postdata(); ?>
+						</div>
 					</div>
 				</div>
+
+				<div class="hidden-entries" style="display:none;"></div>
 			</div>
 
-			<div class="hidden-entries" style="display:none;"></div>
+			<?php
+			$total_pages = $posts->max_num_pages;
+			if ($total_pages > 1){ ?> 
+			<div class="loadmorediv text-center">
+				<div class="wrapper"><a href="#" id="nextPostsBtn" data-current="1" data-baseurl="<?php echo $currentPageLink ?>" data-end="<?php echo $total_pages?>" class="btn-sm wide"><span>Load More Venue</span></a></div>
+			</div>
+			<?php } ?>
 
-		</div>
-		<?php } ?>
-
-		<?php if ($total > $perpage) { ?> 
-		<div class="loadmorediv text-center">
-			<div class="wrapper"><a href="#" id="nextPostsBtn" data-current="1" data-baseurl="<?php echo $currentPageLink ?>" data-end="<?php echo $total_pages?>" class="btn-sm wide"><span>Load More Festivals</span></a></div>
-		</div>
 		<?php } ?>
 
 		<?php
