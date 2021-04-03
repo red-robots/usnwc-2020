37f232917c95ab9218550cd56fb8896fe1391b50
Lisa DeBona <annalisa.quiamco@gmail.com>
2021-04-03 03:58:08 -0400

    Added published date on stories page

--- parts/content-post.php
+++ parts/content-post.php
@@ -15,6 +15,9 @@
 						<div class="inside fadeIn wow" data-wow-delay="<?php echo $sec;?>s">
 							<div class="wrap">
 								<h2 class="title"><?php echo $title; ?></h2>
+								<?php $postdate = get_the_date('M j, Y'); ?>
+								<div class="store-postdate"><span><?php echo $postdate ?></span></div>
+
 								<?php if ($content) { ?>
 									<div class="text">
 										<?php //echo shortenText(strip_tags($content),200,' ','...'); ?>
