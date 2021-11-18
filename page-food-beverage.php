6837be6e9c9df3bd3816757042b8488985b1a8ce
accrane <austincrane@gmail.com>
2021-11-18 13:36:59 -0500

    add new buttons

--- page-food-beverage.php
+++ page-food-beverage.php
@@ -57,12 +57,15 @@ get_header(); ?>
 			<section class="menu-sections">
 				<div class="columns-2">
 				<?php $i=1; foreach ($sections as $s) { 
+					// echo '<pre>';
+					// print_r($s);
+					// echo '<pre>';
 					$e_title = $s['entrytitle'];
 					$e_text = $s['entrytext'];
 					$e_time = $s['entrytime'];
-					$linktype = ($s['entry_linktype']) ? $s['entry_linktype'] : '';
-					$link_field = ($linktype) ? 'entry_'.$linktype : '';
-					$link_val = ($link_field) ? $s[$link_field] : '';
+					// $linktype = ($s['entry_linktype']) ? $s['entry_linktype'] : '';
+					// $link_field = ($linktype) ? 'entry_'.$linktype : '';
+					// $link_val = ($link_field) ? $s[$link_field] : '';
 					$slides = $s['entryslides'];
 					$boxClass = ( ($e_title || $e_text) && $slides ) ? 'half':'full';
 					if( ($e_title || $e_text) || $slides) {  $colClass = ($i % 2) ? ' odd':' even'; ?>
@@ -96,25 +99,10 @@ get_header(); ?>
 											</div>
 										<?php } ?>
 
-										<?php /* Button */ ?>
-										<?php if ($linktype=='pdf' && $link_val ) { 
-											$button_name = $link_val['buttonName'];
-											$button_link = '';
-											if( isset($link_val['pdflink']) && $link_val['pdflink'] ) {
-												$pdf_info = $link_val['pdflink'];
-												$button_link = $pdf_info['url'];
-											}
-											if($button_name && $button_link) { ?>
-												<div class="buttondiv">
-													<a href="<?php echo $button_link ?>" target="_blank" class="btn-sm xs pdflink"><span><?php echo $button_name ?></span></a>
-												</div>
-											<?php } ?>
-										<?php } else if($linktype=='link' && $link_val) { 
-											$button_target = ( isset($link_val['target']) ) ? $link_val['target']:'_self'; ?>
-											<div class="buttondiv">
-												<a href="<?php echo $link_val['url'] ?>" target="<?php echo $button_target ?>" class="btn-sm pagelink"><span><?php echo $link_val['title'] ?></span></a>
-											</div>
-										<?php } ?>
+										<?php 
+											include('parts/fb-button-multiple.php'); 
+											//include('parts/fb-button-single.php'); 
+										?>
 									</div><!-- .info -->
 
 								</div><!-- .inside -->
