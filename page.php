72cf99147ba37c1abee2e704c7dfc232dc1db670
accrane <austincrane@gmail.com>
2022-02-10 13:13:12 -0500

    fix waiver

--- page.php
+++ page.php
@@ -15,7 +15,14 @@
 $placeholder = THEMEURI . 'images/rectangle.png';
 $banner = get_field("flexslider_banner");
 $has_banner = ($banner) ? 'hasbanner':'nobanner';
-get_header(); ?>
+get_header(); 
+
+if( is_page('waiver') ) {
+	$pageClass = 'waiver';
+} else {
+	$pageClass = '';
+}
+?>
 
 <div id="primary" class="content-area-full content-default page-default-template <?php echo $has_banner ?>">
 	<main id="main" class="site-main wrapper" role="main">
@@ -27,7 +34,9 @@ get_header(); ?>
 					<div class="page-header">
 						<h1 class="page-title"><?php the_title(); ?></h1>
 					</div>
-					<?php the_content(); ?>
+					<div class="<?php echo $pageClass; ?>">
+						<?php the_content(); ?>
+					</div>
 				</div>
 			</section>
 
--- wpgulp.config.js
+++ wpgulp.config.js
@@ -12,7 +12,7 @@
 // Local project URL of your already running WordPress site.
 // > Could be something like "wpgulp.local" or "localhost"
 // > depending upon your local WordPress setup.
-const projectURL = 'usnwc.test';
+const projectURL = 'wpgulp.local';
 
 // Theme/Plugin URL. Leave it like it is; since our gulpfile.js lives in the root folder.
 const productURL = './';
@@ -98,8 +98,8 @@ const zipIgnoreGlob = [
 	'!./vscode',
 	'!./package.json',
 	'!./package-lock.json',
-	'!./assets/sass/**/*',
-	'!./assets/sass',
+	'!./assets/css/**/*',
+	'!./assets/css',
 	'!./assets/img/raw/**/*',
 	'!./assets/img/raw',
 	`!${imgSRC}`,
