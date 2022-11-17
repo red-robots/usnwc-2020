965bf58fddec369391fa7b12764f14ac57cec6fc
accrane <austincrane@gmail.com>
2022-11-17 11:57:57 -0500

    e portal

--- page-employment-landing.php
+++ page-employment-landing.php
@@ -32,6 +32,49 @@ if( is_page('waiver') ) {
 					<div class="<?php echo $pageClass; ?>">
 						<?php the_content(); ?>
 					</div>
+					<?php if( have_rows('icons') ) : ?>
+						<div class="cf-forms">
+							<?php while( have_rows('icons') ) : the_row();
+							$title = get_sub_field('title', 'option');
+							$icon = get_sub_field('icon', 'option');
+							$link = get_sub_field('link', 'option');
+							// $pdf = get_sub_field('pdf', 'option');
+							// $fOrLink = get_sub_field('form_or_link', 'option');
+							
+							// if( $fOrLink == 'link' ) {
+							// 	$nLink = $link;
+							// } else {
+							// 	$nLink = $pdf;
+							// }
+							// echo '<pre>';
+							// print_r($title);
+							// echo '</pre>';
+							 
+
+					?>
+
+						
+							<div class="cf-form">
+								<a href="<?php echo $link['url']; ?>" target="<?php echo $link['target']; ?>">
+									<?php if( $icon ) { ?>
+										<div class="cf-icon">
+											<img src="<?php echo $icon['url']; ?>">
+										</div>
+									<?php } ?>
+									<?php if( $title ) { ?>
+										<h3 class="cf-title">
+											<?php echo $title; ?>
+										</h3>
+									<?php } ?>
+								</a>
+							</div>
+						
+
+
+					<?php endwhile; ?>
+					</div>
+					<?php 
+					endif; // end repeater loop ?>
 				</div>
 			</section>
 
