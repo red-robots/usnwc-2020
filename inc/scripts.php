183b509173109f6a104a60b7a63ebbb4220ff776
accrane <austincrane@gmail.com>
2023-03-06 12:42:25 -0500

    close child nav button

--- inc/scripts.php
+++ inc/scripts.php
@@ -7,7 +7,7 @@ function bellaworks_scripts() {
 		'bellaworks-style',
 		 get_stylesheet_uri(),
 		 array(),
-		 '1.88'
+		 '1.89'
 	);
 
 	wp_deregister_script('jquery');
@@ -67,7 +67,7 @@ function bellaworks_scripts() {
 	wp_enqueue_script( 
 			'bellaworks-custom', 
 			get_template_directory_uri() . '/assets/js/custom.js', 
-			array(), '2.25', 
+			array(), '2.26', 
 			true 
 		);
 
