10b54f8652261b7ac8f9b090185fcc50c29f02b1
accrane <austincrane@gmail.com>
2023-06-13 10:52:57 -0400

    add schedule days

--- single-dining.php
+++ single-dining.php
@@ -94,6 +94,91 @@ if( $passport == 'all' ) {
 
 				<?php get_template_part("parts/subpage-tabs"); ?>
 
+				<?php 
+				function sortCourseByDay($courses) {
+				    // Get the current day of the week (0 = Sunday, 1 = Monday, etc.)
+				    $currentDayOfWeek = date('w');
+
+				    // Split the array into two parts: the days before the current day and the days after
+				    $daysBeforeCurrentDay = array_slice($courses, $currentDayOfWeek);
+				    $daysAfterCurrentDay = array_slice($courses, 0, $currentDayOfWeek);
+
+				    // Combine the two parts and return the sorted array
+				    $sortedCourses = array_merge($daysBeforeCurrentDay, $daysAfterCurrentDay);
+
+				    return $sortedCourses;
+				}
+
+				$myDays = get_field('schedule_days');
+
+				$sortedCourses = sortCourseByDay($myDays);
+				// echo '<pre>';
+				// print_r($myDays);
+				// echo '</pre>';
+
+				$num = count($myDays);
+				// echo '<pre>';
+				// print_r($num);
+				// echo '</pre>';
+				if( $num > 3 ) {
+					$dayClass = 'flexslider-instr flexslider carousel';
+				} else {
+					$dayClass = 'flex';
+				}
+
+
+				if( have_rows('schedule_days') ): ?>
+					<section class="instr-schedule">
+						<div class="wwrapper">
+							<div class="shead-icon text-center">
+								<h2 class="stitle"><img src="<?php bloginfo('template_url'); ?>/images/icons/bw_calendar2.svg" width="30"  /> Upcoming</h2>
+							</div>
+						</div>
+						<div id="inst-sched" class="<?php echo $dayClass; ?>">
+							<ul class="slides">
+							<?php foreach($sortedCourses as $day) { 
+								$courseinfo = $day['coursetime'];
+								$day_image = $day['day_image'];
+
+							?>
+								<li id="<?php echo $day['day_name'] ?>" class="slide-item ">
+									<div class="day">
+										<?php if($day_image){ ?>
+											<div class="image">
+												<img src="<?php echo $day_image['url']; ?>">
+											</div>
+										<?php } ?>
+										<div class="contents js-blocks">
+											<h3><?php echo $day['day_name'] ?></h3>
+											<?php foreach( $courseinfo as $c ) { 
+												$product_link = $c['product_link'];
+												$idArray = array('266','267','268','269','270','271','154','152','153','40','41','42','43','58','57','56','55','54','53','59','179','180','38','39');
+												if( $product_link == 'all' ) {
+													$pp = 'data-accesso-launch';
+												} elseif(in_array($product_link, $idArray )) {
+													$pp = 'data-accesso-package="'.$product_link.'"';
+												} else {
+													$pp = 'data-accesso-keyword="'.$product_link.'"';
+												}
+												?>
+												<div class="row">
+													<div class="left"><?php echo $c['time']; ?></div>
+													<div class="right">
+														<?php if($product_link){ echo '<a '.$pp.' href="#">'; } ?>
+														<?php echo $c['course']; ?>
+														<?php if($product_link){ echo '</a>'; }?>
+													</div>
+												</div>
+											<?php } ?>
+										</div>
+									</div>
+								</li>
+							<?php } ?>
+							</ul>
+						</div>
+					</section>
+				<?php endif; ?>
+
 				<?php  
 				/* EVENT DETAILS */
 				$details = get_field("event_details");
